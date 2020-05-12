dfcf22630244c5f6ef2d957f5f6bfd578529cee4
Mateus Bombassaro <mpinho@fesctecnologia.com.br>
2020-05-12 19:47:23 -0300

    busca por logs no backend e armazenamento na store a partir da tela de dashboard

--- central-erros-frontend/src/views/Dashboard.vue
+++ central-erros-frontend/src/views/Dashboard.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="about">
+  <div class="page">
     <h1>Aqui será a dashboard</h1>
-    {{ logs }}
+    <div v-for="log in logs" :key="log.id">
+      <div class="log-container">
+        <p>ID: {{ log.id }}</p>
+        <p>TITULO: {{ log.titulo }}</p>
+        <p>DETALHES: {{ log.detalhes }}</p>
+        <p>LEVEL: {{ log.level }}</p>
+        <p>LOCAL: {{ log.local }}</p>
+        <p>ORIGEM: {{ log.origem }}</p>
+        <p>FREQUENCIA: {{ log.frequencia }}</p>
+        <p>DATA: {{ log.data }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,13 +22,19 @@ import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'Dashboard',
   created() {
-    this.createLog();
+    this.loadLogs();
   },
   computed: {
     ...mapGetters('Logs', ['logs']),
   },
   methods: {
-    ...mapActions('Logs', ['createLog']),
+    ...mapActions('Logs', ['loadLogs']),
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.log-container {
+  margin-bottom: 40px;
+}
+</style>
