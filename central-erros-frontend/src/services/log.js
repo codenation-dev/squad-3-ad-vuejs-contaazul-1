9bc67d08026290693cf30fd202eacaff3766f60d
Mateus Bombassaro <mpinho@fesctecnologia.com.br>
2020-05-12 19:54:02 -0300

    alterado url

--- central-erros-frontend/src/services/log.js
+++ central-erros-frontend/src/services/log.js
@@ -4,5 +4,5 @@ import { api } from 'env';
 export const loadLogs = async userToken => {
   const headers = { Authorization: `Bearer ${userToken}` };
 
-  return await axios.get(`${api}/log?locais=1&locais=2&locais=3`, { headers });
+  return await axios.get(`${api}/log`, { headers });
 };
