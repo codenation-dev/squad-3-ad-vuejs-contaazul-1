8c497580f75cd47f21e4050eb6cbfc662ca0cbde
Eduardo Marchiori <eduardo.cmr@hotmail.com>
2020-05-25 21:36:45 -0300

    adicionado tela de lista de erros

---
+++ central-erros-frontend/src/views/ListaDeErros.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <input v-model="filterLevel" value="1" type="checkbox" />
+        <label for>Produção</label>
+        <input v-model="filterLevel" value="2" type="checkbox" />
+        <label for>Homologação</label>
+        <input v-model="filterLevel" value="3" type="checkbox" />
+        <label for>Dev</label>
+      </div>
+    </div>
+    <lista-de-logs :log-list="logList"></lista-de-logs>
+  </div>
+</template>
+
+<script>
+import ListaDeLogs from '@/components/ListaDeLogs';
+import { mapActions, mapGetters } from 'vuex';
+export default {
+  name: 'ListaDeErros',
+  components: {
+    'lista-de-logs': ListaDeLogs,
+  },
+  data() {
+    return {
+      filterLevel: [],
+    };
+  },
+  async created() {
+    await this.loadLogs();
+    console.log(this.formatedLogs);
+  },
+  computed: {
+    ...mapGetters('Logs', ['formatedLogs', 'filteredLogs']),
+    logList() {
+      if (this.filterLevel.length != 0) {
+        this.addFilterLevel(this.filterLevel);
+        return this.filteredLogs;
+      }
+
+      return this.formatedLogs;
+    },
+  },
+  methods: {
+    ...mapActions('Logs', ['loadLogs', 'addFilterLevel']),
+  },
+};
+</script>
+
+<style></style>
