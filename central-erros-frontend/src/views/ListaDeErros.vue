2c0b7a1ac11d7a84fd0ce39b9b5fcd6319fb189d
Carlos Pecher <carlos.pecher@mosistemas.com>
2020-06-01 22:11:21 -0300

    ajuste no layout

--- central-erros-frontend/src/views/ListaDeErros.vue
+++ central-erros-frontend/src/views/ListaDeErros.vue
@@ -1,62 +1,60 @@
 <template>
   <div>
     <div class="grid-container mt-3em ">
-      <div class="is-width-60">
-        <div class="row">
-          <div class="col-50 jc-start">
-            <div class="row">
-              <div class="mr-1em">
-                <input
-                  v-model="filterLevel"
-                  class="checkbox"
-                  value="1"
-                  type="checkbox"
-                />
-                <label for> Produção</label>
-              </div>
+      <div class="is-width-60 row">
+        <div class="col-50 jc-start">
+          <div class="row">
+            <div class="mr-1em">
+              <input
+                v-model="filterLevel"
+                class="checkbox"
+                value="1"
+                type="checkbox"
+              />
+              <label for> Produção</label>
+            </div>
 
-              <div class="mr-1em">
-                <input
-                  v-model="filterLevel"
-                  class="checkbox"
-                  value="2"
-                  type="checkbox"
-                />
-                <label for> Homologação</label>
-              </div>
+            <div class="mr-1em">
+              <input
+                v-model="filterLevel"
+                class="checkbox"
+                value="2"
+                type="checkbox"
+              />
+              <label for> Homologação</label>
+            </div>
 
-              <div>
-                <input
-                  v-model="filterLevel"
-                  class="checkbox"
-                  value="3"
-                  type="checkbox"
-                />
-                <label for> Dev</label>
-              </div>
+            <div>
+              <input
+                v-model="filterLevel"
+                class="checkbox"
+                value="3"
+                type="checkbox"
+              />
+              <label for> Dev</label>
             </div>
           </div>
-          <div class="col-25 mr-1em">
-            <vue-multiselect
-              v-model="filterColumnListLogs"
-              :options="columnListLogs"
-              :searchable="false"
-              :close-on-select="true"
-              :preselect-first="true"
-              :custom-label="customLabelName"
-              :show-labels="false"
-              placeholder="Buscar por"
-            ></vue-multiselect>
-          </div>
-          <div class="col-25">
-            <input class="input" v-model="filterSearch" type="text" />
-          </div>
+        </div>
+        <div class="col-25 mr-1em">
+          <vue-multiselect
+            v-model="filterColumnListLogs"
+            :options="columnListLogs"
+            :searchable="false"
+            :close-on-select="true"
+            :preselect-first="true"
+            :custom-label="customLabelName"
+            :show-labels="false"
+            placeholder="Buscar por"
+          ></vue-multiselect>
+        </div>
+        <div class="col-25">
+          <input class="input" v-model="filterSearch" type="text" />
         </div>
       </div>
-
-      <lista-de-logs :log-list="logList" v-if="!arquivados"></lista-de-logs>
-      <lista-de-logs :log-list="archivedLogList" v-else></lista-de-logs>
     </div>
+
+    <lista-de-logs :log-list="logList" v-if="!arquivados"></lista-de-logs>
+    <lista-de-logs :log-list="archivedLogList" v-else></lista-de-logs>
   </div>
 </template>
 
