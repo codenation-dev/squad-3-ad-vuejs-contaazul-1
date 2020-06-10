484db076140181883e5e0c5e49926493610a889e
Gean Farias <gcar95@hotmail.com>
2020-06-10 15:15:51 -0300

    finalizando alguns aljustes visuais

--- central-erros-frontend/src/views/NotFound.vue
+++ central-erros-frontend/src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page">
     <div class="content-container">
-      <p class="content-title">404</p>
+      <div class="content-title">4 <img src="@/assets/lupa.svg" /> 4</div>
       <p class="content-info">
         A página que você está buscando não foi encontrada
       </p>
@@ -35,20 +35,30 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    border: 1px solid #e6e6e6;
     border-radius: 8px;
     padding: 30px;
     background-color: #fff;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.51);
+    box-shadow: 0px 0px 7px -3px rgba(0, 0, 0, 0.51);
   }
 }
 
 .content-title {
   font-size: 64px;
-  color: #1a2e69;
+  color: #3e5959;
+  position: relative;
+  width: 265px;
+  display: flex;
+  justify-content: space-evenly;
+  img {
+    width: 85px;
+    margin-top: 20px;
+    position: absolute;
+    margin-left: -12px;
+    pointer-events: none;
+  }
 }
 
 .content-info {
-  margin-bottom: 20px;
+  margin: 20px 0;
 }
 </style>
